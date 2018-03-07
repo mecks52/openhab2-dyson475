01a136dba27c9504fe7847c107f871b5f38aa04e
mecks52 <mecks52@gmx.de>
2018-03-07 18:56:08 +0100

    Some changes after testing script and finding some issues.

--- prepare.py
+++ prepare.py
@@ -14,35 +14,28 @@ pwd = input("Product WiFi Password (e.g.: adgjsfhk):")
 ip_addr = input("Type in the IP-Adress of the air purifier (e.g. 192.168.178.100):")
 
 # Transfer ssid to device identification
-dev_id = ssid.lstrip("DYSON-").rstrip("-475")
-
+dev_id = ssid[6:21]
 # Transfer password to hash version
 hash = hashlib.sha512()
 hash.update(pwd.encode('utf-8'))
 pwd_hash = base64.b64encode(hash.digest()).decode('utf-8')
 
-# Open files read/write access
-mqtt_cfg = open("./mqtt.cfg", 'r+')
-dyson475_items = open("./dyson475.items", 'r+')
-
 # Read in configuration files
+mqtt_cfg = open("./mqtt.cfg", 'r')
+dyson475_items = open("./dyson475.items", 'r')
 mqtt_cfg_str = mqtt_cfg.read()
 dyson475_items_str = dyson475_items.read()
+mqtt_cfg.close()
+dyson475_items.close()
 
-# Replace placeholder <dev-id> with actual device identification
-mqtt_cfg_str.replace("<dev-id>", dev_id)
-dyson475_items_str.replace("<dev-id>", dev_id)
-
-# Replace placeholder <pwd-hash> with actual password hash
-mqtt_cfg_str.replace("<pwd-hash>", pwd_hash)
-
-# Replace placeholder <ip-addr> with actual ip address
-mqtt_cfg_str.replace("<ip-addr>", ip_addr)
-
-# Write files back
-mqtt_cfg.write(mqtt_config_str)
+# Replace placeholder and write back information into files
+mqtt_cfg_str = mqtt_cfg_str.replace("<dev-id>", dev_id)
+dyson475_items_str = dyson475_items_str.replace("<dev-id>", dev_id)
+mqtt_cfg_str = mqtt_cfg_str.replace("<pwd-hash>", pwd_hash)
+mqtt_cfg_str = mqtt_cfg_str.replace("<ip-addr>", ip_addr)
+mqtt_cfg = open("./mqtt.cfg", 'w')
+dyson475_items = open("./dyson475.items", 'w')
+mqtt_cfg.write(mqtt_cfg_str)
 dyson475_items.write(dyson475_items_str)
-
-# Close files
 mqtt_cfg.close()
 dyson475_items.close()
