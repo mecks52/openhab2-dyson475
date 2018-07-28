baedd92e2a97d6fb14a44a512ec78f9ddcaca47a
mecks52 <mecks52@gmx.de>
2018-07-28 09:35:15 +0200

    Refactored prepare script to be more common.

--- prepare.py
+++ prepare.py
@@ -9,7 +9,8 @@ import hashlib
 # Ask for information from device
 print("Which type of Dyson air purifier do you use?\n")
 print(" 455 - Pure Hot + Cool Link\n")
-print(" 475 - Pure Cool Link\n")
+print(" 469 - Pure Cool Link\n")
+print(" 475 - Pure Cool Link Tower\n")
 ap_type = input("type (e.g. 455):")
 print("Type in the information from the sticker inside your\n")
 print("Dyson air purifier:")
@@ -24,48 +25,24 @@ hash = hashlib.sha512()
 hash.update(pwd.encode('utf-8'))
 pwd_hash = base64.b64encode(hash.digest()).decode('utf-8')
 
-if ap_type == "455":
-    # Read in configuration files
-    mqtt_cfg = open("./mqtt455.cfg", 'r')
-    dyson455_items = open("./dyson455.items", 'r')
-    mqtt_cfg_str = mqtt_cfg.read()
-    dyson455_items_str = dyson455_items.read()
-    mqtt_cfg.close()
-    dyson455_items.close()
-
-    # Replace placeholder and write back information into files
+# Create mqtt.cfg file
+mqtt_cfg = "./mqtt"+ap_type+".cfg"
+with open(mqtt_cfg, 'r') as mqtt_cfg_f:
+    mqtt_cfg_str = mqtt_cfg_f.read()
     mqtt_cfg_str = mqtt_cfg_str.replace("<dev-id>", dev_id)
-    dyson455_items_str = dyson455_items_str.replace("<dev-id>", dev_id)
     mqtt_cfg_str = mqtt_cfg_str.replace("<pwd-hash>", pwd_hash)
     mqtt_cfg_str = mqtt_cfg_str.replace("<ip-addr>", ip_addr)
-    mqtt_cfg = open("./mqtt.cfg", 'w')
-    dyson455_items = open("./dyson455.items", 'w')
-    mqtt_cfg.write(mqtt_cfg_str)
-    dyson455_items.write(dyson455_items_str)
-    mqtt_cfg.close()
-    dyson455_items.close()
+mqtt_cfg_f.close()
+with open("mqtt.cfg", 'w') as mqtt_cfg_f:
+    mqtt_cfg_f.write(mqtt_cfg_str)
+mqtt_cfg_f.close()
 
-elif ap_type == "475":
-    # Read in configuration files
-    mqtt_cfg = open("./mqtt475.cfg", 'r')
-    dyson475_items = open("./dyson475.items", 'r')
-    mqtt_cfg_str = mqtt_cfg.read()
-    dyson475_items_str = dyson475_items.read()
-    mqtt_cfg.close()
-    dyson475_items.close()
-
-    # Replace placeholder and write back information into files
-    mqtt_cfg_str = mqtt_cfg_str.replace("<dev-id>", dev_id)
-    dyson475_items_str = dyson475_items_str.replace("<dev-id>", dev_id)
-    mqtt_cfg_str = mqtt_cfg_str.replace("<pwd-hash>", pwd_hash)
-    mqtt_cfg_str = mqtt_cfg_str.replace("<ip-addr>", ip_addr)
-    mqtt_cfg = open("./mqtt.cfg", 'w')
-    dyson475_items = open("./dyson475.items", 'w')
-    mqtt_cfg.write(mqtt_cfg_str)
-    dyson475_items.write(dyson475_items_str)
-    mqtt_cfg.close()
-    dyson475_items.close()
-
-else:
-    print(ap_type + " is not supported!\nOnly 455 and 475 are supported!")
-
+# Create dysonXXX.items file
+dyson_items = "./dyson"+ap_type+".items"
+with open(dyson_items, 'r') as dyson_items_f:
+    dyson_items_str = dyson_items_f.read()
+    dyson_items_str = dyson_items_str.replace("<dev-id>", dev_id)
+dyson_items_f.close()
+with open(dyson_items, 'w') as dyson_items_f:
+    dyson_items_f.write(dyson_items_str)
+dyson_items_f.close()
